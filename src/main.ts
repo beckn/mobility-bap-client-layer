4c8f61515c026554a82a96662d09e31bde048eee
Nikhil Bansal <nikhil@Nikhils-MacBook-Pro.local>
2023-05-22 13:56:55 +0530

    swagger doc added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BAP Client layer')
+    .setDescription('APIs on bap client layer')
+    .setVersion('1.0')
+    .addTag('BAP')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/discovery/discovery.controller.ts
+++ src/modules/discovery/discovery.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SearchRequestDto } from './request/search.request.dto';
 import { ProtocolContextAction } from 'src/shared/models/protocol-context.dto';
 import { DiscoveryService } from './providers/discovery.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Search')
 @Controller('client')
 export class DiscoveryController {
   constructor(
@@ -17,29 +19,3 @@ export class DiscoveryController {
     return await this.discoveryService.search(searchDto);
   }
 }
-
-// val protocolContext =
-//       contextFactory.create(transactionId = request.context.transactionId, action = ProtocolContext.Action.SEARCH)
-//     val loggerRequest = loggingFactory.create(messageId = protocolContext.messageId,
-//       transactionId = protocolContext.transactionId, contextTimestamp = protocolContext.timestamp.toString(),
-//       action = protocolContext.action
-//     )
-//     loggingService.postLog(loggerRequest)
-//     return searchService.search(protocolContext, request.message.criteria)
-//       .fold(
-//         {
-//           log.error("Error during search. Error: {}", it)
-//           val loggerRequest = loggingFactory.create(messageId = protocolContext.messageId, transactionId = protocolContext.transactionId, contextTimestamp = protocolContext.timestamp.toString(),
-//             action = protocolContext.action, bppId = protocolContext.bppId, errorMessage = it.error().message, errorCode = it.error().code
-//           )
-//           loggingService.postLog(loggerRequest)
-//           ResponseEntity
-//             .status(it.status().value())
-//             .body(ProtocolAckResponse(null, it.message(), it.error()))
-//         },
-//         {
-//           log.info("Successfully initiated Search")
-//           ResponseEntity.ok(it)
-//         }
-//       )
-//   }
