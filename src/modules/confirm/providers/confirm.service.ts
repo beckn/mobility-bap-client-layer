07190cb4a3893a1695c387afa3f5e24fc4c5848b
anileminds <anil.rajpoot@eminds.ai>
2023-06-05 10:27:06 +0530

    confirm api changes done

--- src/modules/confirm/providers/confirm.service.ts
+++ src/modules/confirm/providers/confirm.service.ts
@@ -1,10 +1,11 @@
-import { Injectable ,Logger} from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { ConfirmMapper } from "../mapper/confirm.mapper";
 import { ProtocolServerService } from "src/shared/providers/protocol-server.provider";
 import { ConfimRequestDto } from "../request/confirm.request.dto";
 import { becknUrl } from "src/configs/api.config";
-import { ContextFactory } from 'src/shared/factories/context.factory.provider';
+import { ContextFactory } from "src/shared/factories/context.factory.provider";
 import { ProtocolContextAction } from "src/shared/models/protocol-context.dto";
+import { Domain } from "../../../configs/api.config";
 
 @Injectable()
 export class ConfirmService {
@@ -12,37 +13,81 @@ export class ConfirmService {
     private readonly mapper: ConfirmMapper,
     private readonly protocolServerService: ProtocolServerService,
     private readonly contextFactory: ContextFactory,
-    private logger : Logger
-  ){}
+    private logger: Logger
+  ) {}
 
   async confirm(requestPayload: ConfimRequestDto): Promise<any> {
     try {
-      const context = this.contextFactory.create(ProtocolContextAction.CONFIRM,requestPayload.context.domain)
-      context.bpp_id=requestPayload.context.bpp_id
-       context.bpp_uri=requestPayload.context.bpp_uri
-       context.transaction_id=requestPayload.context.transaction_id
-      const paylaod = {
-        context: context,
-        message: {
-        order:
-        {
-            id:requestPayload.message.order.id,
-            provider:requestPayload.message.order.provider,
-            items:requestPayload.message.order.items,
-            quote:requestPayload.message.order.quote,
-            fulfillment:requestPayload.message.order.fulfillment
-        }
-        }
+      const context = this.contextFactory.create(
+        ProtocolContextAction.CONFIRM,
+        requestPayload.context.domain
+      );
+      context.bpp_id = requestPayload.context.bpp_id;
+      context.bpp_uri = requestPayload.context.bpp_uri;
+      context.transaction_id = requestPayload.context.transaction_id;
+
+      let paylaod;
+      if (
+        requestPayload.context.domain === Domain.retail ||
+        requestPayload.context.domain === Domain.tourism
+      ) {
+        let items: any = [];
+        requestPayload.message.order.items.map((item) => {
+          items.push({
+            id: item.id,
+            extended_attributes: {},
+            quantity: item.quantity,
+            price:item.price,
+            descriptor:item.descriptor,
+            tags:item.tags
+          });
+        });
+        paylaod = {
+          context: context,
+          message: {
+            order: {
+              provider: {
+                id:requestPayload.message.order.items[0].provider.id,
+                locations: requestPayload.message.order.items[0].locations
+              },
+              items:items,
+              addOns: [],
+              offers: [],
+              billing:requestPayload.message.order.billing,            
+              fulfillment:requestPayload.message.order.fulfillment,
+              payment:requestPayload.message.order.payment
+            },
+          },
+        };
+      } else {
+        paylaod = {
+          context: context,
+          message: {
+            order: {
+              id: requestPayload.message.order.id,
+              provider: requestPayload.message.order.provider,
+              items: requestPayload.message.order.items,
+              quote: requestPayload.message.order.quote,
+              fulfillment: requestPayload.message.order.fulfillment,
+            },
+          },
+        };
       }
 
-      console.log("Input::",requestPayload)
-      this.logger.log("calling confirm endpoint of protocol server: requestpayload",requestPayload)
-      const result = await this.protocolServerService.executeAction(becknUrl.confirm, paylaod)
-      const mappedResult = this.mapper.map(result)
-      return mappedResult
+      console.log("Input::", requestPayload);
+      this.logger.log(
+        "calling confirm endpoint of protocol server: requestpayload",
+        requestPayload
+      );
+      const result = await this.protocolServerService.executeAction(
+        becknUrl.confirm,
+        paylaod
+      );
+      const mappedResult = this.mapper.map(result);
+      return mappedResult;
     } catch (error) {
-      this.logger.error("error executing searching point",error)
-      throw error
+      this.logger.error("error executing searching point", error);
+      throw error;
     }
   }
 }
--- src/modules/init/providers/init.service.ts
+++ src/modules/init/providers/init.service.ts
@@ -3,7 +3,7 @@ import { InitMapper } from "../mapper/init.mapper";
 import { ProtocolServerService } from "src/shared/providers/protocol-server.provider";
 import { InitRequestDto } from "../request/init.request.dto";
 import { becknUrl } from "src/configs/api.config";
-import { ContextFactory } from 'src/shared/factories/context.factory.provider';
+import { ContextFactory } from "src/shared/factories/context.factory.provider";
 import { ProtocolContextAction } from "src/shared/models/protocol-context.dto";
 import { Domain } from "../../../configs/api.config";
 
@@ -13,70 +13,71 @@ export class InitService {
     private readonly mapper: InitMapper,
     private readonly protocolServerService: ProtocolServerService,
     private readonly contextFactory: ContextFactory
-  ){}
+  ) {}
 
   async init(requestPayload: InitRequestDto): Promise<any> {
     try {
-      const context = this.contextFactory.create(ProtocolContextAction.INIT,requestPayload.context.domain)
-       
-      context.bpp_id=requestPayload.context.bpp_id
-       context.bpp_uri=requestPayload.context.bpp_uri
-       context.transaction_id=requestPayload.context.transaction_id
+      const context = this.contextFactory.create(
+        ProtocolContextAction.INIT,
+        requestPayload.context.domain
+      );
 
-       console.log(requestPayload)
-       let payload
-       if( requestPayload.context.domain === Domain.retail ||
-        requestPayload.context.domain === Domain.tourism){
+      context.bpp_id = requestPayload.context.bpp_id;
+      context.bpp_uri = requestPayload.context.bpp_uri;
+      context.transaction_id = requestPayload.context.transaction_id;
 
-          let items: any = [];
+      console.log(requestPayload);
+      let payload;
+      if (
+        requestPayload.context.domain === Domain.retail ||
+        requestPayload.context.domain === Domain.tourism
+      ) {
+        let items: any = [];
         requestPayload.message.order.items.map((item) => {
           items.push({
             id: item.id,
             quantity: item.quantity,
           });
         });
-          payload = {
-            context: context,
-           message:{
-            order:{
-              
+        payload = {
+          context: context,
+          message: {
+            order: {
               provider: {
-                id:requestPayload.message.order.items[0].provider.id,
-                locations: requestPayload.message.order.items[0].locations
+                id: requestPayload.message.order.items[0].provider.id,
+                locations: requestPayload.message.order.items[0].locations,
               },
-              items:items,
+              items: items,
               addOns: [],
               offers: [],
-              billing:requestPayload.message.order.billing,            
-              fulfillment:requestPayload.message.order.fulfillment,
-            }
-            
-           }
-          }
-       }else{
-         payload = {
-          context: context,
-         message:{
-          order:{
-            
-            provider:requestPayload.message.order.provider,
-            fulfillment:requestPayload.message.order.fulfillment,
-            items:requestPayload.message.order.items,
-             billing:requestPayload.message.order.billing
-             
-  
+              billing: requestPayload.message.order.billing,
+              fulfillment: requestPayload.message.order.fulfillment,
+            },
           },
-          
-         }
-        }
-       }
-       
-      console.log("beck::",payload)
-      const result = await this.protocolServerService.executeAction(becknUrl.init, payload)
-      const mappedResult = this.mapper.map(result)
-      return mappedResult
+        };
+      } else {
+        payload = {
+          context: context,
+          message: {
+            order: {
+              provider: requestPayload.message.order.provider,
+              fulfillment: requestPayload.message.order.fulfillment,
+              items: requestPayload.message.order.items,
+              billing: requestPayload.message.order.billing,
+            },
+          },
+        };
+      }
+
+      console.log("beck::", payload);
+      const result = await this.protocolServerService.executeAction(
+        becknUrl.init,
+        payload
+      );
+      const mappedResult = this.mapper.map(result);
+      return mappedResult;
     } catch (error) {
-      throw error
+      throw error;
     }
   }
 }
