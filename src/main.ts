626f906a1d057ad6376ca882b932de88bb5b7ba5
Zoya <zoya@eminds.ai>
2024-02-27 14:43:04 +0530

    remove cloudwatch logs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import {
 } from 'nest-winston';
 
 import * as winston from 'winston';
-import * as CloudWatchTransport from 'winston-cloudwatch';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { ValidationPipe } from '@nestjs/common/pipes';
@@ -21,24 +20,7 @@ async function bootstrap() {
           winston.format.ms(),
           nestWinstonModuleUtilities.format.nestLike()
         ),
-      }),
-      new CloudWatchTransport({
-        retentionInDays:30,
-        jsonMessage:true,
-        name: "Cloudwatch Logs",
-        logGroupName: `${process.env.CLOUDWATCH_GROUP_NAME}/${process.env.STAGE}`,
-        logStreamName: process.env.CLOUDWATCH_STREAM_NAME,
-        awsAccessKeyId: process.env.AWS_ACCESS_KEY,
-        awsSecretKey: process.env.AWS_KEY_SECRET,
-        awsRegion: process.env.CLOUDWATCH_AWS_REGION,
-        awsOptions: {credentials: {accessKeyId:process.env.AWS_ACCESS_KEY,secretAccessKey:process.env.AWS_KEY_SECRET,},region:process.env.CLOUDWATCH_AWS_REGION},
-
-        messageFormatter: function (item) {
-          return (
-            {level: item.level,  message: item.message , stack:item.stack}.toString()
-          );
-        },
-      }),
+      })
     ],
   }),
   cors:true
@@ -58,7 +40,7 @@ SwaggerModule.setup('api', app, document,{
   }
 },);
   
-await app.listen(3000);
+await app.listen(8000);
 
 }
 
