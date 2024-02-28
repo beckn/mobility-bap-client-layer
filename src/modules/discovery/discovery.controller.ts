6716257d130ba82336d93e3e61ed43f18ebfe7e7
shreyvishal <shreya.vishal@eminds.ai>
2024-02-28 18:35:35 +0530

    Added: Some Comment

--- src/modules/discovery/discovery.controller.ts
+++ src/modules/discovery/discovery.controller.ts
@@ -1,21 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { SearchRequestDto } from './request/search.request.dto';
-import { ProtocolContextAction } from 'src/shared/models/protocol-context.dto';
-import { DiscoveryService } from './providers/discovery.service';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { SearchRequestDto } from "./request/search.request.dto";
+import { ProtocolContextAction } from "src/shared/models/protocol-context.dto";
+import { DiscoveryService } from "./providers/discovery.service";
+import { ApiTags } from "@nestjs/swagger";
 
-@ApiTags('search')
-@Controller('client')
+@ApiTags("search")
+@Controller("client")
 export class DiscoveryController {
-  constructor(
-    private readonly discoveryService: DiscoveryService
-  ) {
+  constructor(private readonly discoveryService: DiscoveryService) {}
 
-  }
-  
-  @Post('/v2/search')
+  @Post("/v2/search")
   async search(@Body() searchDto: SearchRequestDto): Promise<any> {
-    console.log(searchDto)
+    console.log("SearchDto=====>", searchDto);
     return await this.discoveryService.search(searchDto);
   }
 }
